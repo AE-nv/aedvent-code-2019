1b3a5d04886b990f844e26e9771b916bae01956a
Vincent Robaye <vincent.robaye@ae.be>
2019-12-02 20:58:41 +0100

    Day #2 - logging improvement

--- day 02/vince - scala/Runner.scala
+++ day 02/vince - scala/Runner.scala
@@ -24,13 +24,10 @@ object Runner extends Assertions with Matchers {
     for (r <- managed(Source.fromFile("input.txt"))) {
       val input = r.mkString.split(",").map(_.toInt).toList
       println("Part #1: " + part_1(input.updated(1, 12).updated(2, 2)).head)
-      println("Part #2 - (0, 0): " + part_1(input.updated(1, 0).updated(2, 0)).head)
-      println("Part #2 - (1, 0): " + part_1(input.updated(1, 1).updated(2, 0)).head)
-      println("Part #2 - (2, 0): " + part_1(input.updated(1, 2).updated(2, 0)).head)
-      println("Part #2 - (3, 0): " + part_1(input.updated(1, 3).updated(2, 0)).head)
-      println("Part #2 - (1, 1): " + part_1(input.updated(1, 3).updated(2, 1)).head)
-      println("Part #2 - (1, 2): " + part_1(input.updated(1, 3).updated(2, 2)).head)
-      println("Part #2 - (1, 3): " + part_1(input.updated(1, 3).updated(2, 3)).head)
+
+      for (i <- 0 to 3 ; j <- 0 to 3) {
+        println("Part #2 - (%s, %s): %s".format(i, j, part_1(input.updated(1, i).updated(2, j)).head))
+      }
       println("Part #2 - Noun: " + (19690720 - 797822) / (1028222 - 797822))
       println("Part #2 - Verb: " + (19690720 - 797822) % (1028222 - 797822))
       println("Part #2 - (82, 98): " + part_1(input.updated(1, 82).updated(2, 98)).head)
@@ -43,5 +40,4 @@ object Runner extends Assertions with Matchers {
     part_1(List(2, 4, 4, 5, 99, 0)) shouldBe List(2, 4, 4, 5, 99, 9801)
     part_1(List(1, 1, 1, 4, 99, 5, 6, 0, 99)) shouldBe List(30, 1, 1, 4, 2, 5, 6, 0, 99)
   }
-
 }
