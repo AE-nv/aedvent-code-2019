ec06057024ac560f339fedd9f954c7790fce613c
Jeroen Haegebaert <jeroen@haegebaert.com>
2019-12-03 10:47:58 +0100

    c# template

---
+++ dependencies/jeroenh/Specs.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace AdventOfCode
+{
+    public class Specs
+    {
+        string[] input = new[] { "" };
+
+        [Fact]
+        public void TestPart1()
+        {
+            var result = AoC.Part1(input);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var result = AoC.Part2(input);
+        }
+    }
+}
