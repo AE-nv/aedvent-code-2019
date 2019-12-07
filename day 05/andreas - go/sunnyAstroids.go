4ddc5cfc218d4f21d27df80aba56584795bbb12e
andreasDL <andreasdelille@hotmail.com>
2019-12-07 18:05:42 +0100

    refactor wip

--- day 05/andreas - go/sunnyAstroids.go
+++ day 05/andreas - go/sunnyAstroids.go
@@ -21,54 +21,45 @@ func parseInput(input string) []int{
     return codes
 }
 
-func retrieve(data []int, param, param_mode int) (int, bool) {
-    if param_mode == 0 {
-        if param > len(data) { return 0, false }
-        return data[param], true
-    }
-    return param, true
+func retrieve(data []int, param, param_mode int) int {
+    if param_mode == 0 { return data[param] }
+    return param
 }
-func store(data []int, param, param_mode, value int) ([]int, bool){
+func store(data []int, param, param_mode, value int) []int {
     if param_mode == 0 {
-        if param > len(data) { return data, false }
         data[param] = value
-        return data, true
-    } else {
-        panic("you cannot do immidiate with safe opcode")
+        return data
     }
-
-    return data, false
+    panic("you cannot do immidiate with safe opcode")
+    return data
 }
 
-func add(data []int, pos int, param_modi []int) ([]int, bool) {
-    val_a, ok_a := retrieve(data, data[pos+1], param_modi[0])
-    if ! ok_a { return data, false }
-    val_b, ok_b := retrieve(data, data[pos+2], param_modi[1])
-    if ! ok_b { return data, false }
-
+func add(data []int, pos int, param_modi []int) []int {
+    val_a := retrieve(data, data[pos+1], param_modi[0])
+    val_b := retrieve(data, data[pos+2], param_modi[1])
     result := val_a + val_b
 
     return store(data, data[pos+3], param_modi[2], result)
 }
-func multiply(data []int, pos int, param_modi []int) ([]int, bool) {
-    val_a, ok_a := retrieve(data, data[pos+1], param_modi[0])
-    if ! ok_a { return data, false }
-    val_b, ok_b := retrieve(data, data[pos+2], param_modi[1])
-    if ! ok_b { return data, false }
-
+func multiply(data []int, pos int, param_modi []int) []int {
+    val_a := retrieve(data, data[pos+1], param_modi[0])
+    val_b := retrieve(data, data[pos+2], param_modi[1])
     result := val_a * val_b
 
     return store(data, data[pos+3], param_modi[2], result)
 }
-func input(data []int, pos int, param_modi []int) ([]int, bool){
+func input(data []int, pos int, param_modi []int) []int {
     return store(data, data[pos+1], param_modi[0], input_value)
 }
-func output(data []int, pos int, param_modi []int) ([]int, bool){
-    val_a, ok := retrieve(data, data[pos+1], param_modi[0])
-    if ! ok { return data, false }
+func output(data []int, pos int, param_modi []int) []int {
+    val_a := retrieve(data, data[pos+1], param_modi[0])
     fmt.Println("==>>", val_a)
-    return data, true
+    return data
 }
+func jit(data []int, pos int, param_modi []int) []int { return data }
+func jif(data []int, pos int, param_modi []int) ([]int){ return data}
+func lt(data []int, pos int, param_modi []int) ([]int){ return data}
+func eq(data []int, pos int, param_modi []int) ([]int){return data}
 
 func parseInstruction(code int) (int, []int){
     opcode := code % 100
@@ -83,51 +74,53 @@ func parseInstruction(code int) (int, []int){
     }
     return opcode, parameter_modi
 }
-func run(data []int) ([]int, bool) {
-    ok := true
+func run(data []int) ([]int) {
+    fmt.Println()
+    fmt.Println()
+    fmt.Println()
+    fmt.Println(data)
+
     var opcode int
     var param_modi []int
-    for pos := 0; ok && opcode != 99 ; {
+
+    for pos := 0; opcode != 99 ; {
         opcode, param_modi = parseInstruction(data[pos])
 
-        //fmt.Println("\t", pos, opcode, param_modi)
+        //fmt.Println("\t", pos, opcode, param_modi, opcode != 99)
         if opcode == 1 {
-            data, ok = add(data, pos, param_modi)
+            data = add(data, pos, param_modi)
             pos += 4
         } else if opcode == 2 {
-            data, ok = multiply(data, pos, param_modi)
+            data = multiply(data, pos, param_modi)
             pos += 4
         } else if opcode == 3 {
-            data, ok = input(data, pos, param_modi)
+            data = input(data, pos, param_modi)
             pos += 2
         } else if opcode == 4 {
-            data, ok = output(data, pos, param_modi)
+            data = output(data, pos, param_modi)
             pos += 2
         }
-
-        //fmt.Println(data, ok)
-
+        //fmt.Println("\t=>", data, opcode, opcode != 99)
     }
-    return data, ok
+    return data
 }
 
 
 func tests(){
-    result, ok := run(parseInput("1,9,10,3,2,3,11,0,99,30,40,50"))
-    fmt.Println(result, ok)
-    result, ok = run(parseInput("1,0,0,0,99"))
-    fmt.Println(result, ok)
-    result, ok = run(parseInput("2,3,0,3,99"))
-    fmt.Println(result, ok)
-    result, ok = run(parseInput("2,4,4,5,99,0"))
-    fmt.Println(result, ok)
-    result, ok = run(parseInput("1,1,1,4,99,5,6,0,99"))
-    fmt.Println(result, ok)
-
-    result, ok = run(parseInput("1002,4,3,4,33"))
-    fmt.Println(result, ok)
-    result, ok = run(parseInput("1101,100,-1,4,0"))
-    fmt.Println(result, ok)
+    result := run(parseInput("1,9,10,3,2,3,11,0,99,30,40,50"))
+    fmt.Println(result)
+    result  = run(parseInput("1,0,0,0,99"))
+    fmt.Println(result)
+    result  = run(parseInput("2,3,0,3,99"))
+    fmt.Println(result)
+    result  = run(parseInput("2,4,4,5,99,0"))
+    fmt.Println(result)
+    result  = run(parseInput("1,1,1,4,99,5,6,0,99"))
+    fmt.Println(result)
+    result  = run(parseInput("1002,4,3,4,33"))
+    fmt.Println(result)
+    result  = run(parseInput("1101,100,-1,4,0"))
+    fmt.Println(result)
 }
 
 
@@ -138,6 +131,5 @@ func main(){
     tests()
     input_value = 1
     program := parseInput(readInput("input.txt"))
-    result, ok := run(program)
-    fmt.Println(result, ok)
+    fmt.Println(run(program))
 }
