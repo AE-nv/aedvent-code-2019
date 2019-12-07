df916eb33224fa9cfd501f56a6a35d420c99d3a1
mvermeir <mathias.vermeir@gmail.com>
2019-12-08 00:49:02 +0100

    day 4 part 2

--- day 04/mathias - clojure/day4.clj
+++ day 04/mathias - clojure/day4.clj
@@ -3,29 +3,55 @@
 (defn str->numseq [s] (map #(read-string (str %)) s))
 
 (defn double-present?
-  ([previous remaining]
-   (if (seq remaining)
-     (let [current (first remaining)]
-       (if (= previous current)
-         true
-         (double-present? current (rest remaining))))
-     false)))
+      ([previous remaining]
+       (if (seq remaining)
+         (let [current (first remaining)]
+              (if (= previous current)
+                true
+                (double-present? current (rest remaining))))
+         false)))
+
+(defn ascending-numbers? [numseq] (= numseq (sort numseq)))
+(defn number->numseq [x] (str->numseq (str x)))
 
 (defn ok? [x]
-  (let [numseq (str->numseq (str x))]
-    (and (double-present? (first numseq) (rest numseq))
-         (= numseq (sort numseq)))))
+      (let [numseq (number->numseq x)]
+           (and (double-present? (first numseq) (rest numseq))
+                (ascending-numbers? numseq))))
 
 (defn part-1 []
-  (->>
-    (range 124075 580769)
-    (filter ok?)
-    count))
+      (->>
+        (range 124075 580769)
+        (filter ok?)
+        count))
+
+(defn series-size-by-number
+      [numseq]
+      ((reduce
+         (fn [{previous :prev frequencies :freq :as acc} current]
+             (if (= current previous)
+               {:prev current
+                :freq (update frequencies current #(if % (inc %) 2))}
+               {:prev current :freq frequencies}))
+         {}
+         numseq) :freq))
+
+(defn double-present-but-no-more?
+      [numseq]
+      (->> numseq
+           series-size-by-number
+           vals
+           (some #(= 2 %))))
 
 (defn part-2 []
-  "todo")
+      (->>
+        (range 124075 580769)
+        (map number->numseq)
+        (filter ascending-numbers?)
+        (filter double-present-but-no-more?)
+        count))
 
 (defn -main
-  [& args]
-  (println (str "Part 1: " (part-1)))
-  (println (str "Part 2: " (part-2))))
+      [& args]
+      (println (str "Part 1: " (part-1)))
+      (println (str "Part 2: " (part-2))))
