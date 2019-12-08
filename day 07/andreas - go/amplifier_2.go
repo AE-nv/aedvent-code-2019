e6366c3b577f98d783422ae9a07f1e4161d51ecc
andreasDL <andreasdelille@hotmail.com>
2019-12-08 11:42:39 +0100

    part 2 wip, part 1 examples working

--- day 07/andreas - go/amplifier_2.go
+++ day 07/andreas - go/amplifier_2.go
@@ -11,10 +11,8 @@ func readInput(fname string) string{
     text, _ := ioutil.ReadFile(fname)
     return string(text)
 }
-func possibilities(size int) [][]int{
+func possibilities(values []int) [][]int{
     //https://stackoverflow.com/questions/30226438/generate-all-permutations-in-go
-    values := make([]int, size)
-    for i := 0 ; i < size; i++ { values[i] = i }
 
     var helper func([]int, int)
     res := [][]int{}
@@ -57,147 +55,167 @@ func parseInstruction(code int) (int, []int){
     return opcode, parameter_modi
 }
 
-func parseInput(input string) Program{
+func parseInput(input string) Amp{
     parts := strings.Split(input, ",")
     codes := make([]int, len(parts))
     for i, x := range parts {
         codes[i], _ = strconv.Atoi(x)
     }
-    return Program{ codes, []int{} }
+    return Amp{ 0, codes, []int{} }
 }
 
-type Program struct {
+type Amp struct {
+    pos int
     data []int
     input_values []int
 }
 
-func (p Program) retrieve(param, param_mode int) int {
+func (p Amp) retrieve(param, param_mode int) int {
     if param_mode == 0 { return p.data[param] }
     return param
 }
-func (p *Program) store(param, value int) {
+func (p *Amp) store(param, value int) {
     p.data[param] = value
 }
-func (p *Program) inputStream() int{
-    value := p.input_values[len(p.input_values)-1]
-    p.input_values = p.input_values[:len(p.input_values)-1]
+func (p *Amp) inputStream() int{
+    value := p.input_values[0]
+    fmt.Println("reading", value)
+    p.input_values = p.input_values[1:]
     return value
 }
-func (p *Program) outputStream(val int) {
-    p.input_values = append(p.input_values, val)
+func (p *Amp) outputStream(value int) {
+    fmt.Println("storing", value)
+    p.input_values = append(p.input_values, value)
 }
 
-func (p *Program) add(pos int, param_modi []int) int {
-    val_a := p.retrieve(p.data[pos+1], param_modi[0])
-    val_b := p.retrieve(p.data[pos+2], param_modi[1])
+func (p *Amp) add(param_modi []int) {
+    val_a := p.retrieve(p.data[p.pos+1], param_modi[0])
+    val_b := p.retrieve(p.data[p.pos+2], param_modi[1])
     result := val_a + val_b
-    p.store(p.data[pos+3], result)
-    return pos+4
+    p.store(p.data[p.pos+3], result)
+    p.pos+=4
 }
-func (p *Program) multiply(pos int, param_modi []int) int {
-    val_a := p.retrieve(p.data[pos+1], param_modi[0])
-    val_b := p.retrieve(p.data[pos+2], param_modi[1])
+func (p *Amp) multiply(param_modi []int) {
+    val_a := p.retrieve(p.data[p.pos+1], param_modi[0])
+    val_b := p.retrieve(p.data[p.pos+2], param_modi[1])
     result := val_a * val_b
-    p.store(p.data[pos+3], result)
-    return pos+4
+    p.store(p.data[p.pos+3], result)
+    p.pos+=4
 }
-func (p *Program) input(pos int, param_modi []int) int {
+func (p *Amp) input(param_modi []int) {
     value := p.inputStream()
-    p.store(p.data[pos+1], value)
-    return pos+2
+    p.store(p.data[p.pos+1], value)
+    p.pos+=2
 }
-func (p *Program) output(pos int, param_modi []int) int {
-    val_a := p.retrieve(p.data[pos+1], param_modi[0])
+func (p *Amp) output(param_modi []int) int {
+    val_a := p.retrieve(p.data[p.pos+1], param_modi[0])
     p.outputStream(val_a)
-    return pos+2
+    p.pos+=2
+    return val_a
 }
-func (p *Program) jit(pos int, param_modi []int) int {
-    val_a := p.retrieve(p.data[pos+1], param_modi[0])
-    val_b := p.retrieve(p.data[pos+2], param_modi[1])
-    if val_a != 0 { return val_b }
-    return pos+3
+func (p *Amp) jit(param_modi []int) {
+    val_a := p.retrieve(p.data[p.pos+1], param_modi[0])
+    val_b := p.retrieve(p.data[p.pos+2], param_modi[1])
+    if val_a != 0 {
+        p.pos = val_b
+    }else {
+        p.pos+=3
+    }
 }
-func (p *Program) jif(pos int, param_modi []int) int {
-    val_a := p.retrieve(p.data[pos+1], param_modi[0])
-    val_b := p.retrieve(p.data[pos+2], param_modi[1])
-    if val_a == 0 { return val_b }
-    return pos+3
+func (p *Amp) jif(param_modi []int) {
+    val_a := p.retrieve(p.data[p.pos+1], param_modi[0])
+    val_b := p.retrieve(p.data[p.pos+2], param_modi[1])
+    if val_a == 0 {
+        p.pos = val_b
+    } else {
+        p.pos+=3
+    }
 }
-func (p *Program) lt(pos int, param_modi []int) int {
-    val_a := p.retrieve(p.data[pos+1], param_modi[0])
-    val_b := p.retrieve(p.data[pos+2], param_modi[1])
+func (p *Amp) lt(param_modi []int) {
+    val_a := p.retrieve(p.data[p.pos+1], param_modi[0])
+    val_b := p.retrieve(p.data[p.pos+2], param_modi[1])
     result := 0; if val_a < val_b { result = 1 }
-    p.store(p.data[pos+3], result)
-    return pos+4
+    p.store(p.data[p.pos+3], result)
+    p.pos+=4
 }
-func (p *Program) eq(pos int, param_modi []int) int {
-    val_a := p.retrieve(p.data[pos+1], param_modi[0])
-    val_b := p.retrieve(p.data[pos+2], param_modi[1])
+func (p *Amp) eq(param_modi []int) {
+    val_a := p.retrieve(p.data[p.pos+1], param_modi[0])
+    val_b := p.retrieve(p.data[p.pos+2], param_modi[1])
     result := 0; if val_a == val_b { result = 1 }
-    p.store(p.data[pos+3], result)
-    return pos+4
+    p.store(p.data[p.pos+3], result)
+    p.pos+=4
 }
-func (p *Program) run() {
-    pos := 0
+func (p *Amp) run() (int, bool) {
     var opcode int
     var param_modi []int
 
     for opcode != 99 {
-        opcode, param_modi = parseInstruction(p.data[pos])
+        opcode, param_modi = parseInstruction(p.data[p.pos])
 
         if opcode == 1 {
-            pos = p.add(pos, param_modi)
+            p.add(param_modi)
         } else if opcode == 2 {
-            pos = p.multiply(pos, param_modi)
+            p.multiply(param_modi)
         } else if opcode == 3 {
-            pos = p.input(pos, param_modi)
+            p.input(param_modi)
         } else if opcode == 4 {
-            pos = p.output(pos, param_modi)
+            value := p.output(param_modi)
+            return value, false
         } else if opcode == 5 {
-            pos = p.jit(pos, param_modi)
+            p.jit(param_modi)
         } else if opcode == 6 {
-            pos = p.jif(pos, param_modi)
+            p.jif(param_modi)
         } else if opcode == 7 {
-            pos = p.lt(pos, param_modi)
+            p.lt(param_modi)
         } else if opcode == 8 {
-            pos = p.eq(pos, param_modi)
+            p.eq(param_modi)
         } else if opcode == 99 {
-            //will quit eventually
+            return 0, true
         } else {
             panic("what just happend?!")
         }
     }
+    return 0, true
+}
+
+func getOutput(input string, permutation []int) int {
+    amps := []Amp{
+        parseInput(input),
+        parseInput(input),
+        parseInput(input),
+        parseInput(input),
+        parseInput(input),
+    }
+    for i, setting := range permutation {
+        amps[i].outputStream(setting)
+    }
+    amps[0].outputStream(0)
+
+    var output int
+    var done bool
+    for i := 0; i < 5 ; i++ {
+        output, done = amps[i].run()
+        fmt.Println(output, done)
+
+        if i == 5 && done { break }
+        amps[(i+1) % 5].outputStream(output)
+    }
+
+    return output
 }
 
 
 
-func (p *Program) getOutput(settings []int) int{
-    p.input_values = []int{0, settings[0]}
-    p.run()
-    p.input_values = append(p.input_values, settings[1])
-    p.run()
-    p.input_values = append(p.input_values, settings[2])
-    p.run()
-    p.input_values = append(p.input_values, settings[3])
-    p.run()
-    p.input_values = append(p.input_values, settings[4])
-    p.run()
-    return p.input_values[0]
-}
-
-func search(input string) (int, []int) {
-    permutations := possibilities(5)
+func search(input string, permutations [][]int) (int, []int) {
     max_perm, max_val := []int{}, -1
-    for _, permutation := range permutations {
-        program := parseInput(input)
-        value := program.getOutput(permutation)
-        //fmt.Println( i, "/", len(permutations))
+    for i, permutation := range permutations {
 
-        if max_val < 0 || value > max_val {
+        output := getOutput(input, permutation)
+        if max_val < 0 || output > max_val {
             max_perm = permutation
-            max_val = value
+            max_val = output
         }
-
+        fmt.Println( i, "/", len(permutations), permutation, output)
     }
     return max_val, max_perm
 }
@@ -205,21 +223,18 @@ func search(input string) (int, []int) {
 
 
 func main(){
-    /*
-    program := parseInput("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")
-    fmt.Println( getOutput(program, []int{4,3,2,1,0}) )
-
-    program = parseInput("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0")
-    fmt.Println( getOutput(program, []int{0,1,2,3,4}) )
-
-    program = parseInput("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0")
-    fmt.Println( getOutput(program, []int{1,0,4,3,2}) )
 
     input := "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
-    input  = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
-    input  = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
-    */
-    input := readInput("input.txt")
-    max_val, max_perm := search(input)
+    fmt.Println(getOutput(input, []int{4,3,2,1,0}))
+
+    input = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
+    fmt.Println(getOutput(input, []int{0,1,2,3,4}))
+
+    input = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+    fmt.Println(getOutput(input, []int{1,0,4,3,2}))
+    panic("")
+
+    permutations := possibilities([]int{ 5,6,7,8,9 })
+    max_val, max_perm := search(input, permutations)
     fmt.Println(max_val, max_perm)
 }
