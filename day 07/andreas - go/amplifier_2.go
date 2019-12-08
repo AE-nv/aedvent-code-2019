2e63c72de96e46b5d8218503273b85d01ec54563
andreasDL <andreasdelille@hotmail.com>
2019-12-08 12:26:51 +0100

    part 2 samples working

--- day 07/andreas - go/amplifier_2.go
+++ day 07/andreas - go/amplifier_2.go
@@ -228,13 +228,22 @@ func main(){
 
     input = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
     fmt.Println(getOutput(input, []int{1,0,4,3,2}))
-    */
 
     input := "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
     fmt.Println(getOutput(input, []int{ 9,8,7,6,5 }))
 
-    panic("")
+    input := "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
     permutations := possibilities([]int{ 5,6,7,8,9 })
     max_val, max_perm := search(input, permutations)
     fmt.Println(max_val, max_perm)
+
+    input := "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+    permutations := possibilities([]int{ 5,6,7,8,9 })
+    max_val, max_perm := search(input, permutations)
+    fmt.Println(max_val, max_perm)
+    */
+
+
+
+
 }
