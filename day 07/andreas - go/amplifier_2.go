2ed2af0b2a385c7697ab8411a354e2a251c49f21
andreasDL <andreasdelille@hotmail.com>
2019-12-08 12:34:31 +0100

    part 2 done

--- day 07/andreas - go/amplifier_2.go
+++ day 07/andreas - go/amplifier_2.go
@@ -167,7 +167,9 @@ func (p *Amp) run() (int, bool) {
         } else if opcode == 99 {
             return 0, true
         } else {
-            panic("what just happend?!")
+            fmt.Println("invalid opcode")
+            opcode = 99
+            //panic("what just happend?!")
         }
     }
     return 0, true
@@ -218,6 +220,7 @@ func search(input string, permutations [][]int) (int, []int) {
 
 
 func main(){
+    permutations := possibilities([]int{ 5,6,7,8,9 })
 
     /*
     input := "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
@@ -233,17 +236,16 @@ func main(){
     fmt.Println(getOutput(input, []int{ 9,8,7,6,5 }))
 
     input := "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
-    permutations := possibilities([]int{ 5,6,7,8,9 })
     max_val, max_perm := search(input, permutations)
     fmt.Println(max_val, max_perm)
 
     input := "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
-    permutations := possibilities([]int{ 5,6,7,8,9 })
     max_val, max_perm := search(input, permutations)
     fmt.Println(max_val, max_perm)
     */
 
-
-
+    input := readInput("input.txt")
+    max_val, max_perm := search(input, permutations)
+    fmt.Println(max_val, max_perm)
 
 }
