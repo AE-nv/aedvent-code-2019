89b9457e2e5da60f2f000248013275ea8e2a489e
Jo Van Eyck <jo.vaneyck@ae.be>
2019-12-11 15:07:43 +0100

    10.1 actual angle calculation

--- day 10/jo - f#/part1.fsx
+++ day 10/jo - f#/part1.fsx
@@ -9,17 +9,15 @@ let parse lines =
     }
     |> List.ofSeq
 
-type Slope = Infinity | Slope of float
-
-let slope (x1,y1) (x2,y2) : int * int * Slope =
+let angle (x1,y1) (x2,y2) =
     let (dx,dy) = (x2 - x1, y2 - y1)
-    (sign dx, sign dy, if dx = 0 then Infinity else Slope ((float dy)/(float dx)))
+    atan2 (float dy) (float dx)
 
 let solve input =
     let asteroids = parse input
     
     asteroids
-    |> List.map (fun a -> a, asteroids |> List.except [a] |> List.groupBy (slope a))
+    |> List.map (fun a -> a, asteroids |> List.except [a] |> List.groupBy (angle a))
     |> List.map (fun (a,dirs) -> a,dirs |> Seq.length)
     |> List.map snd
     |> List.max
@@ -40,4 +38,4 @@ let t () =
 t ()
 
 let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + "\input.txt") |> List.ofSeq
-input |> solve
+input |> solve //274
