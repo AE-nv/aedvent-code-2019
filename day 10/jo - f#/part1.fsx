96be3dd290fe66edfd279afed2f5c0103b0ede9f
Van Eyck Jo <jo.vaneyck@fluxys.com>
2019-12-11 08:22:25 +0100

    10.1 in F# and a whole lot of vector headscratching

--- day 10/jo - f#/part1.fsx
+++ day 10/jo - f#/part1.fsx
@@ -1,7 +1,5 @@
 #r @"..\..\dependencies\jo\paket\packages\Unquote\lib\net45\Unquote.dll"
-#r @"..\..\dependencies\jo\paket\packages\FsAlg\lib\FSAlg.dll"
 open Swensen.Unquote  
-open FsAlg.Generic
 
 let parse lines = 
     seq {
@@ -11,33 +9,23 @@ let parse lines =
     }
     |> List.ofSeq
 
-let angle a b = 
-    let (x,y),(xb,yb) = a,b
-    let dx,dy = (x - xb), (y - yb)
-    -1
+type Slope = Infinity | Slope of float
 
-let angle (x1,y1) (x2,y2) =
-    let v1 = vector [float x1; float y1]
-    let v2 = vector [float x2; float y2]
-    let dv = v2 - v1
-    let dhorizontal = vector [float System.Int64.MaxValue; float y1]
-    let dot = dhorizontal * dv
-    dot / dhorizontal.
+let slope (x1,y1) (x2,y2) : int * int * Slope =
+    let (dx,dy) = (x2 - x1, y2 - y1)
+    (sign dx, sign dy, if dx = 0 then Infinity else Slope ((float dy)/(float dx)))
 
 let solve input =
     let asteroids = parse input
     
     asteroids
-    |> List.map (fun a -> a, asteroids |> List.except [a] |> List.groupBy (angle a))
+    |> List.map (fun a -> a, asteroids |> List.except [a] |> List.groupBy (slope a))
     |> List.map (fun (a,dirs) -> a,dirs |> Seq.length)
     |> List.map snd
     |> List.max
 
 let t () =
     printf "Testing..."
-    test <@ norm (3,4) = 5.0 @>
-    test <@ normalize (3,4) = (0.6,0.8) @>
-    test <@ dot (1,2) (3,4) = 3 @>
 
     test <@ 
             let example = 
@@ -51,5 +39,5 @@ let t () =
     printfn "..done!"
 t ()
 
-//let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + "\input.txt") |> List.ofSeq
-//input |> solve //WRONG: 265
+let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + "\input.txt") |> List.ofSeq
+input |> solve
