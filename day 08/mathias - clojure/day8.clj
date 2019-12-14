b8f146d44844f8a0d7fc4a5ccdb6af809c18cac1
mvermeir <mathias.vermeir@gmail.com>
2019-12-14 21:08:22 +0100

    day 8 part 2

--- day 08/mathias - clojure/day8.clj
+++ day 08/mathias - clojure/day8.clj
@@ -1,19 +1,27 @@
 (ns aoc-2019.day8
-  (:require [clojure.java.io :as io]))
+    (:require [clojure.java.io :as io]))
 
 (def layers (->> "input-8.txt" io/resource io/reader slurp
                  (partition (* 25 6))))
 
 (defn part-1 []
+      (->> layers
+           (map frequencies)
+           (apply min-key (fn [map] (map \0)))
+           ((fn [map] (* (map \1) (map \2))))))
+
+(def through-section
   (->> layers
-       (map frequencies)
-       (apply min-key (fn [map] (map \0)))
-       ((fn [map] (* (map \1) (map \2))))))
+       (apply interleave)
+       (partition (count layers))))
 
 (defn part-2 []
-  "todo")
+      (let [final-image (->> through-section
+                             (map (partial some #{\0 \1}))
+                             (apply str))]
+           (map println (partition 25 final-image))))
 
 (defn -main
-  [& args]
-  (println (str "Part 1: " (part-1)))
-  (println (str "Part 2: " (part-2))))
+      [& args]
+      (println (str "Part 1: " (part-1)))
+      (println (str "Part 2: " (part-2))))
