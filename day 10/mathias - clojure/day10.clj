bc49d65ca6ec2fbf0c1fb0c8bcfaa86ecb849cd6
mvermeir <mathias.vermeir@gmail.com>
2019-12-26 20:57:28 +0100

    day 10 part 2

--- day 10/mathias - clojure/day10.clj
+++ day 10/mathias - clojure/day10.clj
@@ -1,7 +1,8 @@
 (ns aoc-2019.day10
   (:require [clojure.java.io :as io]
             [clojure.set :as set]
-            [clojure.algo.generic.math-functions :as math]))
+            [clojure.algo.generic.math-functions :as math]
+            [clojure.math.numeric-tower :as num]))
 
 (defn parse-line
   ([line-nb line]
@@ -31,6 +32,14 @@
                           "center"
                           (math/atan2 (- y yc) (- x xc)))))
 
+(defn distance-from [[xc yc :as choosen-center]]
+  (fn [[x y :as point]]
+    (->> (list [xc x] [yc y])
+         (map (partial apply -))
+         (map #(num/expt % 2))
+         (apply +)
+         num/sqrt)))
+
 (defn number-of-asteroids-visible-from [asteroid]
   (->>
     (group-by (angle-with asteroid) all-asteroids)
@@ -43,8 +52,45 @@
        (map number-of-asteroids-visible-from)
        (apply max)))
 
+(defn by-count [asteroid]
+  [(number-of-asteroids-visible-from asteroid) asteroid])
+
+(def the-spot (->> all-asteroids
+                   (map by-count)
+                   (apply max-key first)
+                   second))
+
+(def all-other-asteroids (filter #(not= % the-spot) all-asteroids))
+(def up ((angle-with [0 0]) [0 -1]))
+(def down ((angle-with [0 0]) [0 1]))
+(def right ((angle-with [0 0]) [1 0]))
+(def left ((angle-with [0 0]) [-1 0]))
+
+(def pi (math/atan2 0 -1))
+(def two-pi (* 2 pi))
+
+(defn rotate-up [angle] (+ angle (/ pi 2)))
+(defn angle->positive-angle [angle] (if (neg? angle) (+ two-pi angle) angle))
+(def angle-with-center (comp angle->positive-angle rotate-up (angle-with the-spot)))
+
+(def asteroids-by-angle-and-distance
+  (->> all-other-asteroids
+       (group-by angle-with-center)
+       (into (sorted-map))
+       vals
+       (map #(sort-by (distance-from the-spot) %))))
+
+(defn interleave-all
+  "interleaves including remainder of longer seqs."
+  [& seqs]
+  (if (not-empty (first seqs))
+    (cons (first (first seqs))
+          (lazy-seq (apply interleave-all (filter not-empty (concat (rest seqs) [(rest (first seqs))])))))))
+
+(def destruction-sequence (apply interleave-all asteroids-by-angle-and-distance))
+
 (defn part-2 []
-  "todo")
+  (last (take 200 destruction-sequence)))
 
 (defn -main
   [& args]
